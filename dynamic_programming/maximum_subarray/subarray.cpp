73442a0a69b960fec7f732b5be42ad84ac33dfee
Taskinoor Hasan <taskinoor@gmail.com>
2016-02-21 20:41:38 +0600

    Implement maximum subarray

--- dynamic_programming/maximum_subarray/subarray.cpp
+++ dynamic_programming/maximum_subarray/subarray.cpp
@@ -1,5 +1,27 @@
+#include <algorithm>
+
 #include "subarray.h"
 
 int max_subarray(int *a, int n) {
-    return -1;
+    if (!n) {
+        return 0;
+    }
+
+    int *suffix_sum = new int[n];
+
+    suffix_sum[0] = std::max(0, a[0]);
+
+    for (int i = 1; i < n; i++) {
+        suffix_sum[i] = std::max(0, suffix_sum[i - 1] + a[i]);
+    }
+
+    int max = suffix_sum[0];
+
+    for (int i = 1; i < n; i++) {
+        max = std::max(max, suffix_sum[i]);
+    }
+
+    delete[] suffix_sum;
+
+    return max;
 }

db01c0e29b82ee972718c698097970c7671c158b
Taskinoor Hasan <taskinoor@gmail.com>
2016-03-25 22:28:38 +0600

    Add test case for linked list node

--- data_structure/linked_list/main.cpp
+++ data_structure/linked_list/main.cpp
@@ -1,3 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "list.h"
+
+TEST(NodeTest, DefaultElement) {
+    Node<int> node;
+
+    ASSERT_EQ(0, node.get_element());
+}
+
+TEST(NodeTest, Element) {
+    Node<int> node(2);
+
+    ASSERT_EQ(2, node.get_element());
+}
+
 int main(int argc, char **argv) {
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }

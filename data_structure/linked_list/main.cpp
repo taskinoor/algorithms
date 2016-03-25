02a62c20ed838a99231d55d7d0be0903a6ccb504
Taskinoor Hasan <taskinoor@gmail.com>
2016-03-26 02:33:51 +0600

    Add test cases for basic linked list operations

--- data_structure/linked_list/main.cpp
+++ data_structure/linked_list/main.cpp
@@ -14,6 +14,55 @@ TEST(NodeTest, Element) {
     ASSERT_EQ(2, node.get_element());
 }
 
+TEST(LinkedListTest, EmptyList) {
+    LinkedList<int> list;
+
+    ASSERT_EQ(NULL, list.first());
+    ASSERT_EQ(NULL, list.last());
+}
+
+TEST(LinkedListTest, EmptyInsertFirst) {
+    LinkedList<int> list;
+
+    list.insert_first(2);
+
+    ASSERT_EQ(2, list.first()->get_element());
+    ASSERT_EQ(2, list.last()->get_element());
+}
+
+TEST(LinkedListTest, EmptyInsertLast) {
+    LinkedList<int> list;
+
+    list.insert_last(2);
+
+    ASSERT_EQ(2, list.first()->get_element());
+    ASSERT_EQ(2, list.last()->get_element());
+}
+
+TEST(LinkedListTest, InsertAfter) {
+    LinkedList<int> list;
+
+    list.insert_first(2);
+    list.insert_after(list.first(), 3);
+    list.insert_after(list.last(), 5);
+
+    ASSERT_EQ(2, list.first()->get_element());
+    ASSERT_EQ(3, list.after(list.first())->get_element());
+    ASSERT_EQ(5, list.last()->get_element());
+}
+
+TEST(LinkedListTest, InsertBefore) {
+    LinkedList<int> list;
+
+    list.insert_last(2);
+    list.insert_before(list.last(), 3);
+    list.insert_before(list.first(), 5);
+
+    ASSERT_EQ(2, list.last()->get_element());
+    ASSERT_EQ(3, list.before(list.last())->get_element());
+    ASSERT_EQ(5, list.first()->get_element());
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
 

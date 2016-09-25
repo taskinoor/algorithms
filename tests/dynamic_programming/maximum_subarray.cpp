6e94378ea91bec52713ae6b1b7c192f954298519
Taskinoor Hasan <taskinoor@gmail.com>
2016-09-26 00:53:19 +0600

    Remove unnecessary template specialization while calling max_subarray

--- tests/dynamic_programming/maximum_subarray.cpp
+++ tests/dynamic_programming/maximum_subarray.cpp
@@ -6,31 +6,31 @@ namespace algtest {
 TEST(MaxSubarray, EmptyArray) {
     int a[] = {};
 
-    ASSERT_EQ(0, alg::max_subarray<int>(a, 0));
+    ASSERT_EQ(0, alg::max_subarray(a, 0));
 }
 
 TEST(MaxSubarray, AllPositive) {
     double a[] = {2.2, 3.3, 5.5, 7.7};
 
-    ASSERT_EQ(18.7, alg::max_subarray<double>(a, 4));
+    ASSERT_EQ(18.7, alg::max_subarray(a, 4));
 }
 
 TEST(MaxSubarray, AllNegative) {
     int a[] = {-2, -3, -5, -7};
 
-    ASSERT_EQ(0, alg::max_subarray<int>(a, 4));
+    ASSERT_EQ(0, alg::max_subarray(a, 4));
 }
 
 TEST(MaxSubarray, AllZero) {
     int a[] = {0, 0, 0};
 
-    ASSERT_EQ(0, alg::max_subarray<int>(a, 3));
+    ASSERT_EQ(0, alg::max_subarray(a, 3));
 }
 
 TEST(MaxSubarray, PositiveAndNegative) {
     int a[] = {-2, -4, 3, -1, 5, 6, -7, -2, 4, -3, 2};
 
-    ASSERT_EQ(13, alg::max_subarray<int>(a, 11));
+    ASSERT_EQ(13, alg::max_subarray(a, 11));
 }
 
 }

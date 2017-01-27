f4b554566a5a458d6d6fb2879c56016bd958daa6
Taskinoor Hasan <taskinoor@gmail.com>
2017-01-27 20:59:23 +0600

    Add new test data for maximum subarray from CLRS book

--- tests/dynamic_programming/maximum_subarray.cpp
+++ tests/dynamic_programming/maximum_subarray.cpp
@@ -29,9 +29,13 @@ TEST(MaxSubarray, AllZero) {
 }
 
 TEST(MaxSubarray, PositiveAndNegative) {
-    int a[] = {-2, -4, 3, -1, 5, 6, -7, -2, 4, -3, 2};
+    int a1[] = {-2, -4, 3, -1, 5, 6, -7, -2, 4, -3, 2};
+    int a2[] = {
+        13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7
+    };
 
-    ASSERT_EQ(13, alg::max_subarray(a, 11));
+    ASSERT_EQ(13, alg::max_subarray(a1, 11));
+    ASSERT_EQ(43, alg::max_subarray(a2, 16));
 }
 
 }

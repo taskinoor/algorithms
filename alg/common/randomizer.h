786680be13ac69a640d7e6fb770be3adb81dd44e
Taskinoor Hasan <taskinoor@gmail.com>
2017-03-20 01:40:32 +0600

    Add randomizer utility methods

---
+++ alg/common/randomizer.h
@@ -0,0 +1,17 @@
+#ifndef ALG_RANDOMIZER_H_
+#define ALG_RANDOMIZER_H_
+
+#include <random>
+
+namespace alg {
+namespace randomizer {
+
+std::mt19937& engine();
+
+int uniform_int(int a, int b);
+double uniform_double(double a, double b);
+
+}
+}
+
+#endif

6980b109a47c16126a6d28afbea4c8be5539af22
Taskinoor Hasan <taskinoor@gmail.com>
2017-11-26 15:57:52 +0600

    Move searching codes from .tpp files to .h files

--- alg/searching/linear_search/linear_search.h
+++ alg/searching/linear_search/linear_search.h
@@ -6,10 +6,16 @@
 namespace alg {
 
 template <class T>
-int linear_search(T *a, T key, std::size_t n);
+int linear_search(T *a, T key, std::size_t n) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == key) {
+            return i;
+        }
+    }
 
+    return -1;
 }
 
-#include "linear_search.tpp"
+}
 
 #endif
--- alg/searching/range_query/range_query.h
+++ alg/searching/range_query/range_query.h
@@ -8,14 +8,30 @@
 namespace alg {
 
 template <class T>
-void range_query(BST<T> *bst, T k1, T k2, std::unordered_set<T>& result);
-
-template <class T>
 void range_query(BST<T> *bst, TreeNode<T> *v, T k1, T k2,
-        std::unordered_set<T>& result);
+        std::unordered_set<T>& result) {
 
+    if (v == bst->nil()) {
+        return;
+    }
+
+    if (v->element() < k1) {
+        range_query(bst, bst->right(v), k1, k2, result);
+    } else if (v->element() > k2) {
+        range_query(bst, bst->left(v), k1, k2, result);
+    } else {
+        result.insert(v->element());
+
+        range_query(bst, bst->left(v), k1, k2, result);
+        range_query(bst, bst->right(v), k1, k2, result);
+    }
 }
 
-#include "range_query.tpp"
+template <class T>
+void range_query(BST<T> *bst, T k1, T k2, std::unordered_set<T> &result) {
+    range_query(bst, bst->root(), k1, k2, result);
+}
+
+}
 
 #endif

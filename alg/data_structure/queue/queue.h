c1bf8b0c0e779d0205ec164ece7d68b13c0c845f
Taskinoor Hasan <taskinoor@gmail.com>
2017-11-26 13:23:28 +0600

    Move codes from queue.tpp to queue.h

--- alg/data_structure/queue/queue.h
+++ alg/data_structure/queue/queue.h
@@ -33,8 +33,46 @@ public:
     std::size_t count();
 };
 
+template <class T>
+Queue<T>::Queue(std::size_t n) {
+    size = n + 1;
+    buffer = new T[size];
+    front = 0;
+    rear = 0;
 }
 
-#include "queue.tpp"
+template <class T>
+Queue<T>::~Queue() {
+    delete[] buffer;
+}
+
+template <class T>
+void Queue<T>::enqueue(T element) {
+    if ((rear + 1) % size == front) {
+        throw QueueFullError();
+    }
+
+    buffer[rear] = element;
+    rear = (rear + 1) % size;
+}
+
+template <class T>
+T Queue<T>::dequeue() {
+    if (front == rear) {
+        throw QueueEmptyError();
+    }
+
+    T element = buffer[front];
+    front = (front + 1) % size;
+
+    return element;
+}
+
+template <class T>
+std::size_t Queue<T>::count() {
+    return (rear - front + size) % size;
+}
+
+}
 
 #endif

9424bfdb75e43bcdfeec82a2035156fe77206c12
Taskinoor Hasan <taskinoor@gmail.com>
2017-11-26 15:19:54 +0600

    Move tree traversal codes from .tpp file to .h file

--- alg/data_structure/tree/traversal/euler_tour.h
+++ alg/data_structure/tree/traversal/euler_tour.h
@@ -8,16 +8,34 @@
 namespace alg {
 
 template <class T>
-void euler_visit(TreeNode<T> *v, std::string &out);
+void euler_visit(TreeNode<T> *v, std::string &out) {
+    std::ostringstream buf;
 
-template <class T>
-std::string euler_tour(BinaryTree<T> *tree);
-
-template <class T>
-void euler_tour(BinaryTree<T> *tree, TreeNode<T> *v, std::string &out);
-
+    buf << v->element();
+    out += buf.str();
 }
 
-#include "euler_tour.tpp"
+template <class T>
+std::string euler_tour(BinaryTree<T> *tree) {
+    std::string out = "";
+    euler_tour(tree, tree->root(), out);
+
+    return out;
+}
+
+template <class T>
+void euler_tour(BinaryTree<T> *tree, TreeNode<T> *v, std::string &out) {
+    if (v == tree->nil()) {
+        return;
+    }
+
+    euler_visit(v, out);
+    euler_tour(tree, tree->left(v), out);
+    euler_visit(v, out);
+    euler_tour(tree, tree->right(v), out);
+    euler_visit(v, out);
+}
+
+}
 
 #endif
--- alg/data_structure/tree/traversal/preorder.h
+++ alg/data_structure/tree/traversal/preorder.h
@@ -25,8 +25,52 @@ public:
     TreeNode<T> *current_node() const;
 };
 
+template <class T>
+PreOrderIterator<T>::PreOrderIterator(BinaryTree<T> *tree) {
+    this->tree = tree;
+    stack = new Stack<TreeNode<T> *>(256);
 }
 
-#include "preorder.tpp"
+template <class T>
+PreOrderIterator<T>::~PreOrderIterator() {
+    delete stack;
+}
+
+template <class T>
+void PreOrderIterator<T>::first() {
+    stack->push(tree->root());
+}
+
+template <class T>
+void PreOrderIterator<T>::next() {
+    TreeNode<T> *current = stack->pop();
+    TreeNode<T> *left = tree->left(current);
+    TreeNode<T> *right = tree->right(current);
+    TreeNode<T> *nil_ = tree->nil();
+
+    if (right != nil_) {
+        stack->push(right);
+    }
+    if (left != nil_) {
+        stack->push(left);
+    }
+}
+
+template <class T>
+bool PreOrderIterator<T>::is_done() const {
+    return !stack->count();
+}
+
+template <class T>
+T PreOrderIterator<T>::current_item() const {
+    return stack->top()->element();
+}
+
+template <class T>
+TreeNode<T> *PreOrderIterator<T>::current_node() const {
+    return stack->top();
+}
+
+}
 
 #endif

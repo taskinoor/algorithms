3100a400782b63af9c24d802ad0e89316caa9752
Taskinoor Hasan <taskinoor@gmail.com>
2017-11-26 15:43:49 +0600

    Move maximum subarray(DP) codes from .tpp to .h file

--- alg/dynamic_programming/maximum_subarray/subarray.h
+++ alg/dynamic_programming/maximum_subarray/subarray.h
@@ -8,10 +8,31 @@
 namespace alg {
 
 template <class T>
-T max_subarray_dp(T *a, std::size_t n);
+T max_subarray_dp(T *a, std::size_t n) {
+    if (!n) {
+        return 0;
+    }
 
+    T *suffix_sum = new T[n];
+    T zero = 0;
+
+    suffix_sum[0] = max(zero, a[0]);
+
+    for (int i = 1; i < n; i++) {
+        suffix_sum[i] = max(zero, suffix_sum[i - 1] + a[i]);
+    }
+
+    T max_sum = suffix_sum[0];
+
+    for (int i = 1; i < n; i++) {
+        max_sum = max(max_sum, suffix_sum[i]);
+    }
+
+    delete[] suffix_sum;
+
+    return max_sum;
 }
 
-#include "subarray.tpp"
+}
 
 #endif

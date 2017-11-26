cc4d9582bd000d111a09898539bb2b493efed279
Taskinoor Hasan <taskinoor@gmail.com>
2017-11-26 12:03:03 +0600

    Move contents from utils.tpp to utils.h

--- alg/common/utils.h
+++ alg/common/utils.h
@@ -6,16 +6,32 @@
 namespace alg {
 
 template <class T>
-T max(const T& a, const T& b);
-
-template <class T>
-int min_index(T *a, std::size_t n, int start = 0);
-
-template <class T>
-void swap(T& a, T& b);
-
+T max(const T& a, const T& b) {
+    return a > b ? a : b;
 }
 
-#include "utils.tpp"
+template <class T>
+int min_index(T *a, std::size_t n, int start = 0) {
+    int index = start;
+    T value = a[index];
+
+    for (int i = start + 1; i < n; i++) {
+        if (a[i] < value) {
+            index = i;
+            value = a[index];
+        }
+    }
+
+    return index;
+}
+
+template <class T>
+void swap(T& a, T& b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+}
 
 #endif

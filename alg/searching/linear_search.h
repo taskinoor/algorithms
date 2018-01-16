9861121a63296b5148c1a9806c3b71912cd576d2
Taskinoor Hasan <taskinoor@gmail.com>
2018-01-16 13:24:20 +0600

    Use propert iterator instead of C style array in linear search

--- alg/searching/linear_search.h
+++ alg/searching/linear_search.h
@@ -1,20 +1,18 @@
 #ifndef ALG_SEARCHING_LINEAR_SEARCH_H_
 #define ALG_SEARCHING_LINEAR_SEARCH_H_
 
-#include <cstddef>
-
 namespace alg {
 namespace searching {
 
-template <class T>
-int linear_search(T* a, T key, std::size_t n) {
-    for (int i = 0; i < n; ++i) {
-        if (a[i] == key) {
-            return i;
+template <class InputIt, class T>
+InputIt linear_search(InputIt first, InputIt last, const T& key) {
+    for (InputIt it = first; it != last; ++it) {
+        if (*it == key) {
+            return it;
         }
     }
 
-    return -1;
+    return last;
 }
 
 }
--- tests/searching/linear_search.cpp
+++ tests/searching/linear_search.cpp
@@ -1,3 +1,5 @@
+#include <list>
+
 #include <gtest/gtest.h>
 
 #include "alg/searching/linear_search.h"
@@ -9,7 +11,7 @@ TEST(LinearSearch, KeyExists) {
     int a[n] = {19, 11, 7, 3, 5, 23, 17, 29, 13, 2, 31};
 
     for (int i = 0; i < n; ++i) {
-        ASSERT_EQ(i, alg::searching::linear_search(a, a[i], n));
+        ASSERT_EQ(a + i, alg::searching::linear_search(a, a + n, a[i]));
     }
 }
 
@@ -17,7 +19,20 @@ TEST(LinearSearch, KeyDoesNotExists) {
     constexpr int n = 1;
     double a[n] = {1.618};
 
-    ASSERT_EQ(-1, alg::searching::linear_search(a, 3.14159, n));
+    ASSERT_EQ(a + n, alg::searching::linear_search(a, a + n, 3.14159));
+}
+
+TEST(LinearSearch, SearchWithSTLIterator) {
+    std::list<int> lst = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+    for (const int value : lst) {
+        auto it = alg::searching::linear_search(lst.begin(), lst.end(), value);
+
+        ASSERT_EQ(value, *it);
+    }
+
+    ASSERT_EQ(lst.end(), alg::searching::linear_search(
+            lst.begin(), lst.end(), 29));
 }
 
 }

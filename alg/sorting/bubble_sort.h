b548cffaa2de2408b898d331e3d1ce19278d9179
Taskinoor Hasan <taskinoor@gmail.com>
2018-02-07 02:00:32 +0600

    Use iterator instead of C style interface in bubble sort

--- alg/sorting/bubble_sort.h
+++ alg/sorting/bubble_sort.h
@@ -6,12 +6,12 @@
 namespace alg {
 namespace sorting {
 
-template <class T>
-void bubble_sort(T* a, int n) {
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = n - 1; j > i; --j) {
-            if (a[j] < a[j - 1]) {
-                std::swap(a[j], a[j - 1]);
+template <class RandomIt>
+void bubble_sort(RandomIt first, RandomIt last) {
+    for (RandomIt i = first; last - i > 1; ++i) {
+        for (RandomIt j = last - 1; j > i; --j) {
+            if (*j < *(j - 1)) {
+                std::iter_swap(j, j - 1);
             }
         }
     }
--- tests/sorting/bubble_sort.cpp
+++ tests/sorting/bubble_sort.cpp
@@ -6,29 +6,29 @@
 namespace algtest {
 
 TEST_F(Sorting, BubbleSortEmptyArray) {
-    ASSERT_NO_THROW(alg::sorting::bubble_sort(empty.data(), empty.size()));
+    ASSERT_NO_THROW(alg::sorting::bubble_sort(empty.begin(), empty.end()));
 }
 
 TEST_F(Sorting, BubbleSortSingleElement) {
-    alg::sorting::bubble_sort(single_element.data(), single_element.size());
+    alg::sorting::bubble_sort(single_element.begin(), single_element.end());
 
     ASSERT_EQ(expected_single_element, single_element);
 }
 
 TEST_F(Sorting, BubbleSortAlreadySorted) {
-    alg::sorting::bubble_sort(already_sorted.data(), already_sorted.size());
+    alg::sorting::bubble_sort(already_sorted.begin(), already_sorted.end());
 
     ASSERT_EQ(expected, already_sorted);
 }
 
 TEST_F(Sorting, BubbleSortReverseSorted) {
-    alg::sorting::bubble_sort(reverse_sorted.data(), reverse_sorted.size());
+    alg::sorting::bubble_sort(reverse_sorted.begin(), reverse_sorted.end());
 
     ASSERT_EQ(expected, reverse_sorted);
 }
 
 TEST_F(Sorting, BubbleSortRandomOrdered) {
-    alg::sorting::bubble_sort(random_ordered.data(), random_ordered.size());
+    alg::sorting::bubble_sort(random_ordered.begin(), random_ordered.end());
 
     ASSERT_EQ(expected, random_ordered);
 }
